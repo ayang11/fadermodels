436cb3792bca4ad541e9c3b3c07b1135baefe961
Andrew Yang <ayang11@gmail.com>
2011-07-06 17:50:22 -0400

    Added a tutorial

---
+++ other.R
@@ -0,0 +1,9 @@
+#The was an experimental normal model
+control.norm=function(model,...){
+	return(list(x=model$raw,y=model$raw,names=c('mean','sd'),positives=2,...))
+}
+ll.norm=function(model,param=NULL,x=model$control$x){
+	return(log(dnorm(x,param[1],param[2])))
+}
+mean.norm=function(model) return(sum(model$param$mean*model$param$p))
+vcov.norm=function(model) return(model$param$lambda)
